0adc6e5bef05e91212da507d40fde42c9668bea5
seifeltayeb <seif.eltayeb@gmail.com>
2025-03-08 19:45:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,32 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Set new countdown target: 10 August 2025
+# Set target date (10 Aug 2025)
 TARGET_DATE = datetime(2025, 8, 10, 0, 0)
 
 @app.route('/')
 def index():
     now = datetime.now()
     delta = TARGET_DATE - now
-    time_remaining = {
-        "days": delta.days,
-        "hours": delta.seconds // 3600,
-        "minutes": (delta.seconds // 60) % 60
-    }
-    return render_template("index.html", time=time_remaining)
 
-@app.route('/get_time')
+    time_remaining = {
+        "days": max(delta.days, 0),
+        "hours": max(delta.seconds // 3600, 0),
+        "minutes": max((delta.seconds // 60) % 60, 0),
+    }
+
+    return render_template('index.html', time=time_remaining)
+
+@app.route('/time')
 def get_time():
     now = datetime.now()
     delta = TARGET_DATE - now
-    time_remaining = {
-        "days": delta.days,
-        "hours": delta.seconds // 3600,
-        "minutes": (delta.seconds // 60) % 60
-    }
-    return jsonify(time_remaining)
+
+    return jsonify({
+        "days": max(delta.days, 0),
+        "hours": max(delta.seconds // 3600, 0),
+        "minutes": max((delta.seconds // 60) % 60, 0),
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
